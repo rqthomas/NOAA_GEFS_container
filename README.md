200aff7a80110d26fe37a83b3c4130e3233bd350
rqthomas <rqthomas>
2020-08-25 13:55:26 -0400

    adding README draft

---
+++ README.md
@@ -0,0 +1,13 @@
+NOAA GEFS downloading and temporally (6 hr to 1 hr) downscaling container
+
+`docker pull rqthomas/noaa_gefs_download_downscale:latest`
+
+`bash`
+
+`DIRECTORY_HOST_SHARED=/Users/quinn/Downloads/GEFS_test`
+
+`chmod +x run_noaa_download_downscale_host.sh`
+
+`./run_noaa_download_downscale_host.sh`
+
+
--- run_noaa_download_downscale_host.sh
+++ run_noaa_download_downscale_host.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-run --name noaa_download -e PASSWORD=password -p 8787:8787 -v /Users/quinn/Downloads/GEFS_test:/data rqthomas/noaa_gefs_download_downscale bash /run_noaa_download_downscale.sh
+ docker run --name noaa_download -v /Users/quinn/Downloads/GEFS_test:/data rqthomas/noaa_gefs_download_downscale bash /run_noaa_download_downscale.sh
+ docker container rm noaa_download
