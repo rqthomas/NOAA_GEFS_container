e8a29f4f05c1689002c6ad24119d858f0894da6e
rqthomas <rqthomas>
2020-08-25 13:14:52 -0400

    minor tidyverse fix

--- run_noaa_download_downscale_host.sh
+++ run_noaa_download_downscale_host.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --name flare-dev -e PASSWORD=password -p 8787:8787 -v /Users/quinn/Downloads/GEFS_test:/data rqthomas/noaa_gefs_download_downscale
+run --name noaa_download -e PASSWORD=password -p 8787:8787 -v /Users/quinn/Downloads/GEFS_test:/data rqthomas/noaa_gefs_download_downscale bash /run_noaa_download_downscale.sh
