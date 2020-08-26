21b2f20b74bf6efe745d17b559d6112450e2807d
rqthomas <rqthomas>
2020-08-26 14:14:22 -0400

    cleaning up

--- R/download_downscale_site.R
+++ R/download_downscale_site.R
@@ -1,17 +1,45 @@
-download_downscale_site <- function(site_index,lat.in,lon.in,overwrite,model_name,model_name_ds, output_directory){
+##' @title Download and Downscale NOAA GEFS for a single site
+##' @return None
+##'
+##' @param site_index, full path to 6hr file
+##' @param lat_list, full path to 1hr file that will be generated
+##' @param lon_list, full path to 1hr file that will be generated
+##' @param overwrite, logical stating to overwrite any existing output_file
+##' @param model_name, logical stating to overwrite any existing output_file
+##' @param model_name_ds, logical stating to overwrite any existing output_file
+##' @param output_directory, logical stating to overwrite any existing output_file
+##' @export
+##' 
+##' @author Quinn Thomas
+##' 
+##' 
+
+
+download_downscale_site <- function(site_index, 
+                                    lat_list, 
+                                    lon_list, 
+                                    site_list,
+                                    overwrite, 
+                                    model_name, 
+                                    model_name_ds, 
+                                    output_directory){
   
-  model_dir <- file.path(output_directory,model_name)
+  model_dir <- file.path(output_directory, model_name)
   
-  time <- c(0, 64) #6 hour prediction for 16 days
-  lon.dom <- seq(0, 360, by = 1) #domain of longitudes in model
-  lat.dom <- seq(-90, 90, by = 1) #domain of latitudes in model
+  #Create dimensions of the noaa forecast
+  lon.dom <- seq(0, 359, by = 1) #domain of longitudes in model (1 degree resolution)
+  lat.dom <- seq(-90, 90, by = 1) #domain of latitudes in model (1 degree resolution)
   
-  if(lon.in[site_index] < 0){
-    lon <- which.min(abs(lon.dom  - (360 + lon.in[site_index]))) - 1 #NOMADS indexes start at 0
+  #Convert negetive longitudes to degrees east
+  if(lon_list[site_index] < 0){
+    lon_east <- 360 + lon_list[site_index]
   }else{
-    lon <- which.min(abs(lon.dom  - (lon.in[site_index]))) - 1 #NOMADS indexes start at 0
+    lon_east <- lon_list[site_index]
   }
-  lat <- which.min(abs(lat.dom - lat.in[site_index])) - 1 #NOMADS indexes start at 0 
+  
+  #Find the closed point in the forecast
+  lon <- which.min(abs(lon.dom - lon_east)) - 1 #NOMADS indexes start at 0
+  lat <- which.min(abs(lat.dom - lat_list[site_index])) - 1 #NOMADS indexes start at 0 
   
   #Get yesterdays 6 am GMT, 12 pm GMT, 6 pm GMT, and todays 12 an GMT runs
   urls.out <- rNOMADS::GetDODSDates(abbrev = "gens_bc")
@@ -19,11 +47,11 @@ download_downscale_site <- function(site_index,lat.in,lon.in,overwrite,model_nam
   for(i in 1:length(urls.out$url)){
     
     model.url <- urls.out$url[i]
-    model.url <- stringr::str_replace(model.url,"gens_bc","gens")
-    start_date  <- urls.out$date[i]
-    model_list <- c('gep_all_00z', 'gep_all_06z', 'gep_all_12z', 'gep_all_18z')
+    model.url <- stringr::str_replace(model.url, "gens_bc", "gens")
+    start_date <- urls.out$date[i]
+    model_list <- c("gep_all_00z", "gep_all_06z", "gep_all_12z", "gep_all_18z")
     
-    model_site_date_dir <- file.path(model_dir,site_list[site_index],start_date)
+    model_site_date_dir <- file.path(model_dir, site_list[site_index], start_date)
     
     for(m in 1:length(model_list)){
       
@@ -31,7 +59,7 @@ download_downscale_site <- function(site_index,lat.in,lon.in,overwrite,model_nam
       start_time <- lubridate::as_datetime(start_date) + lubridate::hours(as.numeric(run_hour))
       end_time <- start_time + lubridate::days(16)
       
-      model_site_date_hour_dir <- file.path(model_dir,site_list[site_index],start_date,run_hour)
+      model_site_date_hour_dir <- file.path(model_dir, site_list[site_index], start_date,run_hour)
       if(!dir.exists(model_site_date_hour_dir)){
         dir.create(model_site_date_hour_dir, recursive=TRUE, showWarnings = FALSE)
       }
@@ -46,6 +74,7 @@ download_downscale_site <- function(site_index,lat.in,lon.in,overwrite,model_nam
         print(paste("Downloading", site_list[site_index], format(start_time, "%Y-%m-%dT%H")))
         
         model.runs <- rNOMADS::GetDODSModelRuns(model.url)
+        
         #check if avialable at NOAA
         if(model_list[m] %in% model.runs$model.run){
           
@@ -69,9 +98,17 @@ download_downscale_site <- function(site_index,lat.in,lon.in,overwrite,model_nam
           noaa_data <- list()
           
           for(j in 1:length(noaa_var_names)){
-            noaa_data[[j]] <- rNOMADS::DODSGrab(model.url, model.run, noaa_var_names[j], time = time, lon = c(lon,lon), 
-                                                lat = c(lat,lat),ensembles=c(0,20))
-            noaa_data[[j]]$forecast.date <- lubridate::with_tz(noaa_data[[j]]$forecast.date, tzone = "UTC")
+            
+            noaa_data[[j]] <- rNOMADS::DODSGrab(model.url = model.url, 
+                                                model.run = model.run, 
+                                                noaa_var_names[j] = noaa_var_names[j], 
+                                                time = c(0, 64), 
+                                                lon = lon, 
+                                                lat = lat,
+                                                ensembles=c(0, 20))
+            #For some reason it defaults to the computer's time zone, convert to UTC
+            noaa_data[[j]]$forecast.date <- lubridate::with_tz(noaa_data[[j]]$forecast.date, 
+                                                               tzone = "UTC")
           }
           
           names(noaa_data) <- cf_var_names        
@@ -89,22 +126,27 @@ download_downscale_site <- function(site_index,lat.in,lon.in,overwrite,model_nam
                                           specific_humidity = noaa_data$specific_humidity$value,
                                           cloud_area_fraction = noaa_data$cloud_area_fraction$value)
           
-          
+          #9.999e+20 is the missing value so convert to NA
           forecast_noaa$surface_downwelling_longwave_flux_in_air[forecast_noaa$surface_downwelling_longwave_flux_in_air == 9.999e+20] <- NA
           forecast_noaa$surface_downwelling_shortwave_flux_in_air[forecast_noaa$surface_downwelling_shortwave_flux_in_air == 9.999e+20] <- NA
           forecast_noaa$precipitation_flux[forecast_noaa$precipitation_flux == 9.999e+20] <- NA
           forecast_noaa$cloud_area_fraction[forecast_noaa$cloud_area_fraction == 9.999e+20] <- NA
           
+          #Calculate wind speed from east and north components
           forecast_noaa$wind_speed <- sqrt(forecast_noaa$eastward_wind^2 + forecast_noaa$northward_wind^2)
           
+          #remove the east and north components
           forecast_noaa <- forecast_noaa %>% dplyr::select(-c("eastward_wind","northward_wind"))
           
           # Convert NOAA's total precipitation (kg m-2) to precipitation flux (kg m-2 s-1)
           #NOAA precipitation data is an accumulation over 6 hours.
-          forecast_noaa$precipitation_flux <- udunits2::ud.convert(forecast_noaa$precipitation_flux, "kg m-2 hr-1", "kg m-2 6 s-1")  #There are 21600 seconds in 6 hours
+          forecast_noaa$precipitation_flux <- udunits2::ud.convert(forecast_noaa$precipitation_flux, 
+                                                                   "kg m-2 hr-1", 
+                                                                   "kg m-2 6 s-1")  #There are 21600 seconds in 6 hours
           
           for (ens in 1:21) { # i is the ensemble number
             
+            #Turn the ensemble number into a string
             if(ens< 10){
               ens_name <- paste0("0",ens)
             }else{
@@ -114,8 +156,10 @@ download_downscale_site <- function(site_index,lat.in,lon.in,overwrite,model_nam
             fname <- paste0(identifier,"_ens",ens_name,".nc")
             output_file <- file.path(model_site_date_hour_dir,fname)
             
-            write_noaa_gefs_netcdf(df = forecast_noaa,ens, lat = lat.in[site_index], lon.in[site_index], cf_units = cf_var_units1, output_file = output_file, overwrite = overwrite)
+            #Write netCDF
+            write_noaa_gefs_netcdf(df = forecast_noaa,ens, lat = lat_list[site_index], lon_list[site_index], cf_units = cf_var_units1, output_file = output_file, overwrite = overwrite)
             
+            #Downscale the forecast from 6hr to 1hr
             modelds_site_date_hour_dir <- file.path(output_directory,model_name_ds,site_list[site_index],start_date,run_hour)
             
             if(!dir.exists(modelds_site_date_hour_dir)){
@@ -126,7 +170,8 @@ download_downscale_site <- function(site_index,lat.in,lon.in,overwrite,model_nam
                                    format(end_time, "%Y-%m-%dT%H"), sep="_")
             fname_ds <- file.path(modelds_site_date_hour_dir, paste0(identifier_ds,"_ens",ens_name,".nc"))
             
-            temporal_downscale(input_file = output_file, output_file = fname_ds, overwrite = TRUE)
+            #Run downscaling
+            temporal_downscale(input_file = output_file, output_file = fname_ds, overwrite = overwrite)
           }
         }
       }else{
--- launch_download_downscale.R
+++ launch_download_downscale.R
@@ -1,27 +1,45 @@
+##' @title Script to launch NOAA download and temporal downscaling
+##' @return None
+##' @param none, full path to 6hr file
+##' @export
+##' 
+##' @author Quinn Thomas
+##' 
+##' 
+
 library(rNOMADS)
 library(tidyverse)
 
+#Use these for using code on local machine
 #source("/Users/quinn/Dropbox/Research/EFI_RCN/NOAA_GEFS_download/NOAA_GEFS_container/write_noaa_gefs_netcdf.R")
 #source("/Users/quinn/Dropbox/Research/EFI_RCN/NOAA_GEFS_download/NOAA_GEFS_container/temporal_downscaling.R")
 #source("/Users/quinn/Dropbox/Research/EFI_RCN/NOAA_GEFS_download/NOAA_GEFS_container/download_downscale_site.R")
 #output_directory <- "/Users/quinn/Downloads/GEFS_test"
 #configuration_yaml <- "/Users/quinn/Dropbox/Research/EFI_RCN/NOAA_GEFS_download/noaa_config/Downloads/GEFS_test"
 
+#source files and set paths on container
+#these directories won't change on container
 source("/noaa/write_noaa_gefs_netcdf.R")
 source("/noaa/temporal_downscaling.R")
 source("/noaa/download_downscale_site.R")
 output_directory <- "/data"
 configuration_yaml <- "/noaa_config/noaa_download_scale_config.yml"
 
+
+
+#Set model name
+model_name <- "NOAAGEFS"
+model_name_ds <-"NOAAGEFStimeds" #Downscaled NOAA GEFS
+
+#Read configuration file
 config_file <- yaml::read_yaml(configuration_yaml)
 
+#Read list of latitude and longitudes
 neon_sites <- readr::read_csv(config_file$site_file, col_types = cols())
-model_name <- "NOAAGEFS"
-model_name_ds <-"NOAAGEFStimeds"
-
 site_list <- neon_sites$site_id
-lat.in <- neon_sites$latitude
-lon.in <- neon_sites$longitude
+lat_list <- neon_sites$latitude
+lon_list <- neon_sites$longitude
+
 overwrite <- config_file$overwrite
 
 run_parallel <- config_file$run_parallel
@@ -29,12 +47,14 @@ run_parallel <- config_file$run_parallel
 print(paste0("Site file: ", config_file$site_file))
 print(paste0("Overwrite existing files: ", config_file$overwrite))
 print(paste0("Running in parallel: ", config_file$run_parallel))
-#####
 
 if(run_parallel){
   
+  #Create cluster
   numCores <- config_file$numCores
   if(numCores > parallel::detectCores()){
+    #Docker sets the max number of cores, if the request is for more, set to 
+    #what docker allows
     numCores <- parallel::detectCores()
     
   }
@@ -44,10 +64,12 @@ if(run_parallel){
   
   site_index <- 1:length(site_list)
   
+  #Run download_downscale_site() over the site_index
   parallel::mclapply(X = site_index, 
                      FUN = download_downscale_site, 
-                     lat.in = lat.in,
-                     lon.in = lon.in,
+                     lat_list = lat_list,
+                     lon_list = lon_list,
+                     site_list = site_list,
                      overwrite = overwrite,
                      model_name = model_name,
                      model_name_ds = model_name_ds, 
@@ -59,14 +81,13 @@ if(run_parallel){
   for(site_index in 1:length(site_list)){
     
     download_downscale_site(site_index,
-                            lat.in = lat.in,
-                            lon.in = lon.in,
+                            lat_list = lat_list,
+                            lon_list = lon_list,
+                            site_list = site_list,
                             overwrite = overwrite,
                             model_name = model_name,
                             model_name_ds = model_name_ds,
                             output_directory = output_directory)
-    
   }
-  
 }
 
