9d937b1a718dcd7fd150be7304820d0e11758b25
rqthomas <rqthomas>
2020-08-27 12:23:38 -0400

    moving r code to package

--- Dockerfile
+++ Dockerfile
@@ -11,22 +11,19 @@ RUN apt-get -yq update && \
 	libudunits2-dev \
 	libnetcdf-dev \
 	ssh && \
-	R -e "install.packages(c('rNOMADS', 'RCurl', 'stringr', 'yaml','ncdf4', 'humidity', 'udunits2','yaml'))" && \
+	R -e "install.packages(c('rNOMADS', 'RCurl', 'stringr', 'yaml','ncdf4', 'humidity', 'udunits2','yaml', 'devtools'))" && \
 	wget -O /usr/bin/yq https://github.com/mikefarah/yq/releases/download/3.2.1/yq_linux_amd64 
 	
-RUN mkdir -p /noaa 
+RUN mkdir -p /noaa
 RUN mkdir -p /noaa/R
 
 COPY launch_download_downscale.R /noaa/launch_download_downscale.R
-COPY R/temporal_downscaling.R /noaa/R/temporal_downscaling.R
-COPY R/write_noaa_gefs_netcdf.R /noaa/R/write_noaa_gefs_netcdf.R
-COPY R/download_downscale_site.R /noaa/R/download_downscale_site.R
-COPY R/noaa_gefs_download_downscale.R /noaa/R/noaa_gefs_download_downscale.R
-COPY R/rNOMADS_2.5.0.tar.gz /noaa/R/rNOMADS_2.5.0.tar.gz
+COPY rNOMADS_2.5.0.tar.gz /noaa/R/rNOMADS_2.5.0.tar.gz
 COPY run_noaa_download_downscale.sh /run_noaa_download_downscale.sh
 
 # Get flare-container.sh
-RUN R -e "install.packages('/noaa/R/rNOMADS_2.5.0.tar.gz', repos = NULL)" 
+RUN R -e "install.packages('/noaa/R/rNOMADS_2.5.0.tar.gz', repos = NULL)"
+RUN R -e "devtools::install_github("rqthomas/noaaGEFSpoint")"
 	
 #create directory where output directory will be created	
 RUN mkdir -p /noaa/data
--- launch_download_downscale.R
+++ launch_download_downscale.R
@@ -2,19 +2,12 @@
 library(tidyverse)
 
 #Use these for using code on local machine
-#source("/Users/quinn/Dropbox/Research/EFI_RCN/NOAA_GEFS_download/NOAA_GEFS_container/R/write_noaa_gefs_netcdf.R")
-#source("/Users/quinn/Dropbox/Research/EFI_RCN/NOAA_GEFS_download/NOAA_GEFS_container/R/temporal_downscaling.R")
-#source("/Users/quinn/Dropbox/Research/EFI_RCN/NOAA_GEFS_download/NOAA_GEFS_container/R/download_downscale_site.R")
-#source("/Users/quinn/Dropbox/Research/EFI_RCN/NOAA_GEFS_download/NOAA_GEFS_container/R/noaa_gefs_download_downscale.R")
 #output_directory <- "/Users/quinn/Downloads/GEFS_test"
 #configuration_yaml <- "/Users/quinn/Dropbox/Research/EFI_RCN/NOAA_GEFS_download/NOAA_GEFS_container/example/noaa_download_scale_config.yml"
 
 #source files and set paths on container
 #these directories won't change on container
-source("/noaa/R/write_noaa_gefs_netcdf.R")
-source("/noaa/R/temporal_downscaling.R")
-source("/noaa/R/download_downscale_site.R")
-source("/noaa/R/noaa_gefs_download_downscale.R")
+
 
 output_directory <- "/noaa/data"
 configuration_yaml <- "/noaa/config/noaa_download_scale_config.yml"
@@ -28,22 +21,17 @@ site_list <- neon_sites$site_id
 lat_list <- neon_sites$latitude
 lon_list <- neon_sites$longitude
 
-overwrite <- config_file$overwrite
-
-run_parallel <- config_file$run_parallel
-
-num_cores <- config_file$num_cores
-
-downscale <- config_file$downscale
-
 print(paste0("Site file: ", config_file$site_file))
 
-noaa_gefs_download_downscale(site_list,
-                             lat_list,
-                             lon_list,
-                             output_directory,
-                             downscale,
-                             run_parallel = run_parallel,
-                             num_cores = num_cores, 
-                             overwrite = FALSE)
+noaaGEFSpoint::noaa_gefs_download_downscale(site_list,
+                                            lat_list,
+                                            lon_list,
+                                            output_directory,
+                                            forecast_time = config_file$forecast_time,
+                                            forecast_date = config_file$forecast_date,
+                                            latest = config_file$latest,
+                                            downscale = config_file$downscale,
+                                            run_parallel = config_file$run_parallel,
+                                            num_cores = config_file$num_cores,
+                                            overwrite = config_file$overwrite)
 
